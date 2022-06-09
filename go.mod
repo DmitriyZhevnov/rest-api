d01cbbbd19a55dd8bf85ddcda6b465e20dc816fc
Dmitry Zhevnov <dmitriy.zhevnov04@mail.ru>
2022-06-09 14:16:32 +0300

    Change configs

--- go.mod
+++ go.mod
@@ -3,14 +3,9 @@ module github.com/DmitriyZhevnov/rest-api
 go 1.16
 
 require (
-	github.com/BurntSushi/toml v1.1.0 // indirect
-	github.com/ilyakaznacheev/cleanenv v1.2.6
-	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/sirupsen/logrus v1.8.1
-	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/spf13/viper v1.12.0
 	go.mongodb.org/mongo-driver v1.9.1
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -4,25 +4,33 @@ import (
 	"sync"
 
 	"github.com/DmitriyZhevnov/rest-api/pkg/logging"
-	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/spf13/viper"
 )
 
 type Config struct {
-	IsDebug *bool   `yaml:"is_debug"`
-	Listen  Listen  `yaml:"listen"`
-	MongoDB MongoDB `json:"mongodb"`
+	IsDebug *bool `mapstructure:"is_debug"`
+	Listen  Listen
+	Storage Storage
+	Auth    AuthConfig
 }
 
+type AuthConfig struct {
+	PasswordSalt string
+}
+
+type Storage struct {
+	MongoDB MongoDB `json:"mongodb"`
+}
 type MongoDB struct {
-	Host       string `json:"host"`
-	Port       string `json:"port"`
-	Database   string `json:"database"`
-	Collection string `json:"collection"`
+	Host       string `mapstructure:"host"`
+	Port       string `mapstructure:"port"`
+	Database   string `mapstructure:"database"`
+	Collection string `mapstructure:"collection"`
 }
 
 type Listen struct {
-	BindIP string `yaml:"bind_ip"`
-	Port   string `yaml:"port"`
+	BindIP string `mapstructure:"bind_ip"`
+	Port   string `mapstructure:"port"`
 }
 
 var instance *Config
@@ -32,12 +40,56 @@ func GetConfig() *Config {
 	once.Do(func() {
 		logger := logging.GetLogger()
 		logger.Info("read application configuration")
+
+		if err := setUpViper(); err != nil {
+			logger.Fatal(err)
+		}
+
 		instance = &Config{}
-		if err := cleanenv.ReadConfig("config.yml", instance); err != nil {
-			help, _ := cleanenv.GetDescription(instance, nil)
-			logger.Info(help)
+		if err := unmarshal(instance); err != nil {
+			logger.Fatal(err)
+		}
+
+		if err := fromEnv(instance); err != nil {
 			logger.Fatal(err)
 		}
 	})
 	return instance
+}
+
+func unmarshal(cfg *Config) error {
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return err
+	}
+
+	if err := viper.UnmarshalKey("listen", &cfg.Listen); err != nil {
+		return err
+	}
+
+	if err := viper.UnmarshalKey("stotage.mongodb", &cfg.Storage.MongoDB); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func fromEnv(cfg *Config) error {
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+
+	cfg.Auth.PasswordSalt = viper.GetString("PASSWORD_SALT")
+
+	return nil
+}
+
+func setUpViper() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("main")
+
+	return viper.ReadInConfig()
 }
